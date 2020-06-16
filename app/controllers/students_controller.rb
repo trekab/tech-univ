6b0baac777fbda55b85c14dc5e9b8d0c4c963ead
Treasure Kabareebe <treasurecalm@gmail.com>
2020-06-17 00:31:05 +0300

    Add student show page

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -7,6 +7,11 @@ class StudentsController < ApplicationController
     @student = Student.new
   end
 
+  def show
+    @student = Student.find(params[:id])
+    
+  end
+
   def create
     @student = Student.new(student_params)
     if @student.save
